7cb9718e1da298e7892e5a38e7ddb35d8dd9819d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-06 20:17:45 +0530

    Update JioSavan.py

--- YukkiMusic/platforms/JioSavan.py
+++ YukkiMusic/platforms/JioSavan.py
@@ -1,4 +1,5 @@
 import re
+import yt_dlp
 
 class Saavn:
     def __init__(self):
@@ -7,3 +8,28 @@ class Saavn:
 
     def valid(self, url: str) -> bool:
         return re.match(self.regex, url) is not None
+
+
+    def playlist(playlist_url):
+        ydl_opts = {
+            'extract_flat': True,
+            'force_generic_extractor': True,
+            'quiet': True,
+        }
+        song_info = []
+    
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                playlist_info = ydl.extract_info(playlist_url, download=False)
+                for entry in playlist_info['entries']:
+                    info = {
+                        "title": entry['title'],
+                        "thumbnail": entry.get('thumbnail', '')
+                        "webpage_url": entry['url'],
+                    }
+                    song_info.append(info)
+                return song_info
+            except Exception:
+                pass
+        return song_info
+    

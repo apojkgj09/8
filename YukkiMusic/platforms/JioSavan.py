5ad13dc852488152e9ecd8cf2c3482735bf509df
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-06 19:11:16 +0530

    Update JioSavan.py

--- YukkiMusic/platforms/JioSavan.py
+++ YukkiMusic/platforms/JioSavan.py
@@ -2,7 +2,8 @@ import re
 
 class Saavn:
     def __init__(self):
+        self.base = "https://www.jiosaavn.com/"
         self.regex = r'https?://(www\.)?jiosaavn\.com/(song|featured|shows)/.*'
 
-    def is_valid(self, url: str) -> bool:
+    def valid(self, url: str) -> bool:
         return re.match(self.regex, url) is not None

6b0f97d7b47dfa90678f690b44304d3b6493f71a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-07 06:42:09 +0530

    Update JioSavan.py

--- YukkiMusic/platforms/JioSavan.py
+++ YukkiMusic/platforms/JioSavan.py
@@ -1,6 +1,7 @@
 import re
 import os
 import yt_dlp
+import asyncio
 from config import seconds_to_time
 
 class SaavnAPI:
@@ -8,36 +9,33 @@ class SaavnAPI:
         self.base = "https://www.jiosaavn.com/"
         self.regex = r'https?://(www\.)?jiosaavn\.com/(song|featured|shows)/.*'
 
-    def valid(self, url: str) -> bool:
-        return re.match(self.regex, url) is not None
+    async def valid(self, url: str) -> bool:
+        return await asyncio.get_event_loop().run_in_executor(None, lambda: re.match(self.regex, url) is not None)
 
-    def playlist(self, url):
+    async def playlist(self, url):
         ydl_opts = {
             'extract_flat': True,
             'force_generic_extractor': True,
             'quiet': True,
         }
         song_info = []
+        loop = asyncio.get_event_loop()
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                playlist_info = ydl.extract_info(url, download=False)
-                for entry in playlist_info['entries']:
-                    duration_sec = entry.get('duration', 0)
-                    info = {
-                        "title": entry['title'],
-                        "duration_sec": duration_sec,
-                        "duration_min": seconds_to_time(duration_sec),
-                        "thumbnail": entry.get('thumbnail', ''),
-                        "webpage_url": entry['url'],
-                    }
-                    song_info.append(info)
-            except Exception:
-                pass
+            playlist_info = await loop.run_in_executor(None, ydl.extract_info, url, False)
+            for entry in playlist_info['entries']:
+                duration_sec = entry.get('duration', 0)
+                info = {
+                    "title": entry['title'],
+                    "duration_sec": duration_sec,
+                    "duration_min": seconds_to_time(duration_sec),
+                    "thumbnail": entry.get('thumbnail', ''),
+                    "webpage_url": entry['url'],
+                }
+                song_info.append(info)
         return song_info
 
     @staticmethod
-    def download(url):
-        
+    async def download(url):
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': 'downloads/%(id)s.%(ext)s',
@@ -47,11 +45,11 @@ class SaavnAPI:
             'no_warnings': True,
         }
 
+        loop = asyncio.get_event_loop()
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
+            info = await loop.run_in_executor(None, ydl.extract_info, url, False)
             xyz = os.path.join("downloads", f"{info['id']}.{info['ext']}")
             if os.path.exists(xyz):
                 return xyz
-            ydl.download([url])
+            await loop.run_in_executor(None, ydl.download, [url])
             return xyz
-

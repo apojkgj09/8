cdb572b3e0a194e3312b2d5c6f1885b7d75aed96
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-11 11:33:28 +0530

    Update misc.py

--- YukkiMusic/misc.py
+++ YukkiMusic/misc.py
@@ -15,7 +15,7 @@ import heroku3
 from pyrogram import filters
 
 import config
-from YukkiMusic.core.mongo import mongodb
+from YukkiMusic.core.mongo import pymongodb
 
 from .logging import LOGGER
 
@@ -53,33 +53,30 @@ def dbb():
     LOGGER(__name__).info(f"Database Initialized.")
 
 
-async def sudo():
+
+def sudo():
     global SUDOERS
     OWNER = config.OWNER_ID
     if config.MONGO_DB_URI is None:
         for user_id in OWNER:
             SUDOERS.add(user_id)
     else:
-        sudoersdb = mongodb.sudoers
-        sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+        sudoersdb = pymongodb.sudoers
+        sudoers = sudoersdb.find_one({"sudo": "sudo"})
         sudoers = [] if not sudoers else sudoers["sudoers"]
-        
         for user_id in OWNER:
             SUDOERS.add(user_id)
             if user_id not in sudoers:
                 sudoers.append(user_id)
-                await sudoersdb.update_one(
+                sudoersdb.update_one(
                     {"sudo": "sudo"},
                     {"$set": {"sudoers": sudoers}},
                     upsert=True,
                 )
-        
         if sudoers:
             for x in sudoers:
                 SUDOERS.add(x)
-    
-    LOGGER(__name__).info("Sudoers Loaded.")
-
+    LOGGER(__name__).info(f"Sudoers Loaded.")
 
 def heroku():
     global HAPP

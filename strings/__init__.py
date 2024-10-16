74ed1eecc0b7c1550b66715bc2254eb5ce00497b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-16 15:11:07 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -6,10 +6,9 @@
 # Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved
-
 import os
 import sys
-from typing import List
+from typing import List, Dict
 
 import yaml
 
@@ -19,25 +18,38 @@ commands = {}
 languages_present = {}
 
 
-def get_command(value: str) -> List:
-    return commands["command"][value]
+def get_command(value: str, language: str = "en") -> List[str]:
+    if value not in commands:
+        available_commands = ", ".join(commands.keys())
+        raise ValueError(
+            f"Command '{value}' does not exist. "
+            f"Please ensure the command is defined in the YAML files under './strings/commands/'. "
+            f"Available commands are: {available_commands}"
+        )
+    
+    return commands[value].get(language, [])
 
 
-def command(cmd: str):
-    cmds = " ".join([f"/{c}" for c in get_command(cmd)])
+def command(cmd: str, language: str = "en") -> str:
+    cmds = " ".join([f"/{c}" for c in get_command(cmd, language)])
     return cmds
+
 
 
 def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./strings"):
+for filename in os.listdir(r"./strings/commands"):
     if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
+        language_code = filename[:-4]
+        with open(f"./strings/commands/{filename}", encoding="utf8") as file:
+            language_commands = yaml.safe_load(file)
+        for command_key, command_list in language_commands.items():
+            if command_key not in commands:
+                commands[command_key] = {}
+            commands[command_key][language_code] = command_list
+
 
 
 for filename in os.listdir(r"./strings/langs/"):
@@ -63,3 +75,7 @@ for filename in os.listdir(r"./strings/langs/"):
             "There is some issue with the language file inside bot. Please report it to the TheTeamvk at @TheTeamvk on Telegram"
         )
         sys.exit()
+
+
+
+

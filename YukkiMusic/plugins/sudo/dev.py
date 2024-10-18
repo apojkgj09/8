7d43022d5583b6292c5455ee6f6181f1f01b9618
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-18 09:44:57 +0530

    Update dev.pyk

--- YukkiMusic/plugins/sudo/dev.py
+++ YukkiMusic/plugins/sudo/dev.py
@@ -20,56 +20,57 @@ import traceback
 from inspect import getfullargspec
 from io import StringIO
 from time import time
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 
-
 async def aexec(code, client, message):
+    local_vars = {}
     exec(
         "async def __aexec(client, message): "
-        + "".join(f"\n {a}" for a in code.split("\n"))
+        + "".join(f"\n {a}" for a in code.split("\n")),
+        globals(),
+        local_vars
     )
-    return await locals()["__aexec"](client, message)
-
+    __aexec_func = local_vars["__aexec"]
+    return await __aexec_func(client, message)
 
 async def edit_or_reply(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
-
 @app.on_edited_message(
-    filters.command(["ev", "eval"]) & SUDOERS & ~filters.forwarded & ~filters.via_bot
-)
+    filters.command(["ev", "eval"]) & SUDOERS & ~filters.forwarded & ~filters.via_bot)
 @app.on_message(
-    filters.command(["ev", "eval"]) & SUDOERS & ~filters.forwarded & ~filters.via_bot
-)
+    filters.command(["ev", "eval"]) & SUDOERS & ~filters.forwarded & ~filters.via_bot)
 async def executor(client: app, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>Give me something to exceute</b>")
+        return await edit_or_reply(message, text="<b>Give me something to execute</b>")
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
         return await message.delete()
+    
     t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
+    
     try:
         await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
+    
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
+    
     evaluation = "\n"
     if exc:
         evaluation += exc
@@ -79,7 +80,9 @@ async def executor(client: app, message: Message):
         evaluation += stdout
     else:
         evaluation += "Success"
+    
     final_output = f"<b>RESULTS:</b>\n<pre language='python'>{evaluation}</pre>"
+    
     if len(final_output) > 4096:
         filename = "output.txt"
         with open(filename, "w+", encoding="utf8") as out_file:
